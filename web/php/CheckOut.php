f53d7711e9e412027d7c6d549e1a8d916815feef
Brendan Shaklovitz <nyanshak@users.noreply.github.com>
2014-12-06 17:05:21 -0600

    crossing my fingers

--- web/php/CheckOut.php
+++ web/php/CheckOut.php
@@ -13,83 +13,82 @@
 <body>
 <div id="Wrap">
 <form id='orderform' method='POST' action='/php/PlaceOrder.php'>
+
 <?php
 $_SESSION["Url"] = $_SERVER["REQUEST_URI"];
 define('INCL_HEADER_CONST', TRUE);
 ob_start();
-include ('header.php');
-
+include('header.php');
 $result = ob_get_clean();
 echo $result;
 define('INCL_BASE_CONST', true);
 include ('base.php');
-
 unset($params);
 ?>
 <div id="LeftSidebar">
 </div> <!-- end "LeftSidebar" -->
 <div id="MainContent">
-<?php
-
+<?
 if (!empty($_SESSION['Email'])) {
-	$email = $_SESSION['Email'];
-	$userResults = $mysqli->query("SELECT `Id`,`ShippingAddress` FROM `User` WHERE `Email` = '" . $email . "'");
-	$userArray = mysqli_fetch_array($userResults);
-	$userid = $userArray['Id'];
-	$address = $userArray['ShippingAddress'];
-	$checkOrder = $mysqli->query("SELECT * FROM `Order` WHERE `UserId` = " . $userid . " and `Status` = 'Pending'");
-	if ($checkOrder->num_rows > 0) {
-		while ($order = mysqli_fetch_array($checkOrder)) {
-			$orderId = $order['Id'];
-			$total = $order['Total'];
-			echo "<table class='order'>";
-			echo "<tr class='border'>";
-			echo "<td>Name<br/></td>";
-			echo "<td>Description<br/></td>";
-			echo "<td>Quantity</td>";
-			echo "<td> Price </td>";
-			echo "</tr>";
-			$orderItems = $mysqli->query("SELECT * FROM `OrderItem` WHERE `OrderId` = " . $orderId);
-			$i = 1;
-			setlocale(LC_MONETARY, 'en_US');
-			if ($orderItems->num_rows > 0) {
-				while ($itemDetail = mysqli_fetch_array($orderItems)) {
-					$productId = $itemDetail['ProductId'];
-					$quantity = $itemDetail['Quantity'];
-					$price = $itemDetail['Price'];
-					$productResult = $mysqli->query("SELECT * FROM `Product` WHERE `Id` = " . $productId);
-					$productDetail = mysqli_fetch_array($productResult);
-					$pname = $productDetail['Name'];
-					$description = $productDetail['Description'];
-					$plink = $productDetail['Picture'];
-					echo '<tr>';
-					echo '<td><img src="' . $plink . '" alt="Product Picture"><br/>' . $pname . '</td>';
-					echo '<td>' . $description . '</td>';
-					echo "<td><span id='itemQuantity'>" . $quantity . "</span> <input type='hidden' id='orderId' value=" . $orderId . "> <input type='hidden' id='productId' value=" . $productId . "></td>";
-					echo "<td><span id='newPrice'>" . money_format('%(#5n', $price * $quantity) . '</span></td>';
-					echo '</tr>';
-				}
-			}
+$email = $_SESSION['Email'];
+$userResults = $mysqli->query("SELECT `Id`,`ShippingAddress` FROM `User` WHERE `Email` = '" . $email . "'");
+$userArray = mysqli_fetch_array($userResults);
+$userid = $userArray['Id'];
+$address = $userArray['ShippingAddress'];
+$checkOrder = $mysqli->query("SELECT * FROM `Order` WHERE `UserId` = " . $userid . " and `Status` = 'Pending'");
+if ($checkOrder->num_rows > 0) {
+while($order = mysqli_fetch_array($checkOrder)) {
+$orderId = $order['Id'];
 
-			$stax = .08 * $total;
-			$total = $total + $stax;
+echo "<table class='order'>";
+echo "<tr class='border'>";
+echo "<td>Name<br/></td>";
+echo "<td>Description<br/></td>";
+echo "<td>Quantity</td>";
+echo "<td> Price </td>";
+echo "</tr>";
+$orderItems = $mysqli->query("SELECT * FROM `OrderItem` WHERE `OrderId` = " . $orderId );
+$i = 1;
+$stax=0;
+$total=0;
+if ($orderItems->num_rows > 0) {
 
-			echo "<tr> <td colspan='2'></td><td colspan='2'> Sales Tax: " . money_format('%(#5n', $stax) . "</td></tr>";
-			echo "<tr> <td colspan='2'> <td colspan='2'> Order Total: " . money_format('%(#5n', $total) . "</td><input type='hidden' id='total' value= '" . $total . "'/> </td></tr>";
-			echo "<tr rowspan='2'> <td colspan='2'></td><td colspan='2'> Shipping Address: " . $address . "</td></tr>";
-			echo "<tr>";
-			echo " <td colspan='2'></td>";
-			echo " <td colspan='2'><input type='submit' value='Confirm Order'> &nbsp; <a href ='/php/browse.php' >Continue Shopping</td>";
-			echo "</tr>";
-			echo '</table>';
-		}
-	}
+while ($itemDetail = mysqli_fetch_array($orderItems)) {
+$productId = $itemDetail['ProductId'];
+$quantity = $itemDetail['Quantity'];
+$price = $itemDetail['Price'];
+$productResult = $mysqli->query("SELECT * FROM `Product` WHERE `Id` = " . $productId );
+$productDetail = mysqli_fetch_array($productResult);
+$pname = $productDetail['Name'];
+$description = $productDetail['Description'];
+$plink = $productDetail['Picture'];
+$total=$total+($price*$quantity);    
+echo '<tr>';
+echo '<td><img src="' . $plink . '" alt="Product Picture"><br/>' . $pname . '</td>';
+echo '<td>' . $description . '</td>';
+echo "<td><span id='itemQuantity'>" . $quantity . "</span> <input type='hidden' id='orderId' value=" . $orderId . "> <input type='hidden' id='productId' value=" . $productId . "></td>";
+echo "<td><span id='newPrice'>" . $price * $quantity . '</span></td>';
+echo '</tr>';
 }
-else {
-	echo "<h1>Error</h1>";
-	echo "<p> Please <a href=\"/php/login.php\">click here to sign in</a>.</p>";
 }
 
+$stax = 0.08 * $total;
+$total=$total+$stax;
+   
+echo "<tr> <td colspan='2'></td><td colspan='2'> Sales Tax: " . $stax . "</td></tr>";
+echo "<tr> <td colspan='2'></td>  <td colspan='2'> Order Total: " . $total . "</td></tr>";
+echo "<tr rowspan='2'> <td colspan='2'></td><td colspan='2'> Shipping Address: " . $address . "</td></tr>";
+echo "<tr>";
+echo " <td colspan='2'></td>" ;
+echo " <td colspan='2'> <input type='hidden' id='total' value=" . $total . "><input type='submit' value='Confirm Order'> &nbsp; <a href ='/php/browse.php' >Continue Shopping</td>";
+echo "</tr>";
+echo '</table>';
+}
+}
+} else {
+echo "<h1>Error</h1>";
+echo "<p> Please <a href=\"/php/login.php\">click here to sign in</a>.</p>";
+}
 ?>
 </form>
 </div> <!-- End MainContent -->
--- web/php/ViewCart.php
+++ web/php/ViewCart.php
@@ -16,13 +16,11 @@
 $_SESSION["Url"] = $_SERVER["REQUEST_URI"];
 define('INCL_HEADER_CONST', TRUE);
 ob_start();
-include ('header.php');
-
+include('header.php');
 $result = ob_get_clean();
 echo $result;
 define('INCL_BASE_CONST', true);
 include ('base.php');
-
 unset($params);
 ?>
 <div id="LeftSidebar">
@@ -30,65 +28,58 @@ unset($params);
 <div id="MainContent">
 <form id='myform' method='POST' action='/php/CheckOut.php'>
 <?php
-
 if (!empty($_SESSION['Email'])) {
-	$email = $_SESSION['Email'];
-	$userResults = $mysqli->query("SELECT `Id` FROM `User` WHERE `Email` = '" . $email . "'");
+$email = $_SESSION['Email'];
+$userResults = $mysqli->query("SELECT `Id` FROM `User` WHERE `Email` = '" . $email . "'");
+$userArray = mysqli_fetch_array($userResults);
+$userid = $userArray['Id'];
+$checkOrder = $mysqli->query("SELECT * FROM `Order` WHERE `UserId` = " . $userid . " and `Status` = 'Pending'");
+if ($checkOrder->num_rows > 0) {
+while($order = mysqli_fetch_array($checkOrder)) {
+$orderId = $order['Id'];
+echo '<table class="order">';
+echo '<tr class="border">';
+echo "<td>Name<br/></td>";
+echo "<td>Description<br/></td>";
+echo "<td>Quantity</td>";
+echo "<td> Price </td>";
+echo "</tr>";
+$quantity = 1;
+$orderItems = $mysqli->query("SELECT * FROM `OrderItem` WHERE `OrderId` = " . $orderId );
+if ($orderItems->num_rows > 0) {
+$i = 1;
 
-	$userArray = mysqli_fetch_array($userResults);
+echo "<input type='hidden' id='orderId' value='" . $orderId . "'>";
+while($itemDetail = mysqli_fetch_array($orderItems)){
+$productId = $itemDetail['ProductId'];
+$quantity = $itemDetail['Quantity'];
+$price = $itemDetail['Price'];
+$productResult = $mysqli->query("SELECT * FROM `Product` WHERE `Id` = " . $productId );
+$productDetail = mysqli_fetch_array($productResult);
+$pname = $productDetail['Name'];
+$description = $productDetail['Description'];
+$plink = $productDetail['Picture'];
+echo '<tr>';
+echo '<td><img src="' . $plink . '" alt="Product Picture"><br/>' . $pname . '</td>';
+echo '<td>' . $description . '</td>';
+echo "<td><input type='text' id='itemQuantity" . $i . "' size='2' maxLength='3' value=" . $quantity . ">&nbsp;<a href='' id='updateQuant" . $i . "'> Update </a> <input type='hidden' id='productId' value=" . $productId . "> <input type='hidden' value=".$i."></td>";
 
-	$userid = $userArray['Id'];
-	$checkOrder = $mysqli->query("SELECT * FROM `Order` WHERE `UserId` = " . $userid . " AND `Status` = 'Pending'");
-	if ($checkOrder->num_rows > 0) {
-		while ($order = mysqli_fetch_array($checkOrder)) {
-			$orderId = $order['Id'];
-			echo '<table class="order">';
-			echo '<tr class="border">';
-			echo "<td>Name<br/></td>";
-			echo "<td>Description<br/></td>";
-			echo "<td>Quantity</td>";
-			echo "<td>Price</td>";
-			echo "</tr>";
-			$quantity = 1;
-			$orderItems = $mysqli->query("SELECT * FROM `OrderItem` WHERE `OrderId` = " . $orderId);
-
-			setlocale(LC_MONETARY, 'en_US');
-			if ($orderItems->num_rows > 0) {
-				$i = 1;
-				echo "<input type='hidden' id='orderId' value='" . $orderId . "'>";
-				while ($itemDetail = mysqli_fetch_array($orderItems)) {
-					$productId = $itemDetail['ProductId'];
-					$quantity = $itemDetail['Quantity'];
-					$price = $itemDetail['Price'];
-					$productResult = $mysqli->query("SELECT * FROM `Product` WHERE `Id` = " . $productId);
-					$productDetail = mysqli_fetch_array($productResult);
-					$pname = $productDetail['Name'];
-					$description = $productDetail['Description'];
-					$plink = $productDetail['Picture'];
-					echo '<tr>';
-					echo '<td><img src="' . $plink . '" alt="Product Picture"><br/>' . $pname . '</td>';
-					echo '<td>' . $description . '</td>';
-					echo "<td><input type='text' id='itemQuantity" . $i . "' size='2' maxLength='3' value=" . $quantity . ">&nbsp;<a href='' id='updateQuant" . $i . "'> Update </a> <input type='hidden' id='productId' value=" . $productId . "> <input type='hidden' value=" . $i . "></td>";
-					echo "<td><span id='newPrice'>" . money_format('%(#5n', $price * $quantity) . '</span></td>';
-					echo '</tr>';
-					$i++;
-				}
-			}
-
-			echo "<tr><td colspan='4'> <input type='submit' value='Checkout'></td></tr>";
-			echo '</table>';
-		}
-	}
-	else {
-		echo "<h1>Empty Cart</h1>";
-		echo "<p>Sorry, There are no items in your cart. Please <a href=" / ">click here to look at our catalog</a>.</p>";
-	}
+echo "<td><span id='newPrice'>" . $price * $quantity . '</span></td>';
+echo '</tr>';
+$i++;
 }
-else {
-	echo "<h1>Error</h1>";
-	echo "<p> Please <a href=\"/php/login.php\">click here to sign in</a>.</p>";
 }
-
+echo "<tr><td colspan='4'> <input type='submit' value='Checkout'></td></tr>";
+echo '</table>';
+}
+} else {
+echo "<h1>Empty Cart</h1>";
+echo "<p>Sorry, There are no items in your cart. Please <a href=\"/\">click here to look at our catalog</a>.</p>";
+}
+} else {
+echo "<h1>Error</h1>";
+echo "<p> Please <a href=\"/php/login.php\">click here to sign in</a>.</p>";
+}
 ?>
 </form>
 </div> <!-- End MainContent -->
